d7713f37a34927208010f15f7229750972be34dd
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2026-01-07 23:32:45 +0100

    Add add_expense and list_expenses api

--- lib/expense_tracker_server.ex
+++ lib/expense_tracker_server.ex
@@ -5,6 +5,24 @@ defmodule ExpenseTrackerServer do
         GenServer.start_link(__MODULE__, :ok, opts)
     end
 
+    def add_expense(pid, title, amount, category) do
+        GenServer.call(pid, {:add, title, amount, category})
+    end
+
+    def list_expenses(pid) do
+        GenServer.call(pid, :list)
+    end
+
+    @impl true
+    def handle_call({:add, title, amount, category}, _from, expenses) do
+        new_expenses = Core.add(expenses, title, amount, category)
+        {:reply, :ok, new_expenses}
+    end
+
+    @impl true
+    def handle_call(:list, _from, expenses) do
+        {:reply, Core.list(expenses), expenses}
+    end
     @impl true
     def init(:ok) do
         {:ok, Core.new()}
